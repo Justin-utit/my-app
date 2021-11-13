b9a29e080ec885e09e7536a518514e8b8b12e3a9
Justin-Chen <justin.uob@gmail.com>
2021-11-13 09:55:06 +0800

    11.13

---
+++ src/com/modernjava/homework/inclasspractice/p211113/聯誼話題相親數_1005.java
@@ -0,0 +1,33 @@
+package com.modernjava.homework.inclasspractice.p211113;
+
+/*
+15可被5整除，5就是因數
+例如說 220 的全部正因數（扣掉自己）相加是：1+2+4+5+10+11+20+22+44+55+110 = 284
+而 284 的全部正因數（扣掉自己）相加總和是：1+2+4+71+142 = 220
+ */
+
+public class 聯誼話題相親數_1005 {
+
+    // 其實就是判斷一個數字是不是相親數
+    public static void main(String[] args) {
+        int[] intArr = {220,221,222,223};
+
+        for(int a : intArr){
+            int result = getAmicableNumber(a);
+            if(result==-1)
+                System.out.println("QQ");
+            else
+                System.out.println(result);
+        }
+
+    }
+
+    public static int getAmicableNumber(int num){
+
+
+
+        return 0;
+    }
+
+
+}
